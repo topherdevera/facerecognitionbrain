dc37a6ffd4c702980f5ebd97fafcd945ff562784
Christopher de Vera <topher@fcfminerals.local>
2020-02-29 10:28:18 +0800

    updated as of Feb 29, 2020

--- src/App.js
+++ src/App.js
@@ -1,18 +1,92 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/*
-            <ImageLinkForm />
-            <FaceRecognition />*/}
-    </div>
-  );
+const app =new Clarifai.App({
+	apiKey: 'ffac2d06c3464f3a91133d3d44f37056'
+});
+
+const particlesOptions = {
+	particles: {
+	    number: {
+	        value: 100,
+	        density: {
+	            enable: true,
+	            value_are: 800
+	        }
+	    }
+	}
 }
 
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+			imageUrl: '',
+			route: 'signin',
+			isSignedIn: 'false'
+		}
+	}
+
+	onInputChange = (event) => {
+		this.setState({ input: event.target.value });
+	}
+
+	onButtonSubmit = () => {
+		this.setState({ imageUrl: this.state.input })
+		app.models
+			.predict(
+				 Clarifai.FACE_DETECT_MODEL,
+				 this.state.input)
+			.then(
+		    function(response) {
+		      console.log(response);
+		    },
+		    function(err) {
+		      // there was an error
+		    }
+		  );
+	}
+
+	onRouteChange = (route) => {
+		this.setState({route: route});
+	}
+
+	render() {
+		return (
+		    <div className="App">	
+		    	<Particles className='particles'
+					    params={particlesOptions} 
+				/>
+				    <Navigation onRouteChange={this.onRouteChange}/>
+				    { this.state.route === 'home'  
+				    	?<div>
+				      		<Logo />
+						    <Rank />
+						    <ImageLinkForm 
+							   	onInputChange={this.onInputChange} 
+						      	onButtonSubmit={this.onButtonSubmit} 
+				        />
+				            <FaceRecognition imageUrl={this.state.imageUrl}/>
+		            	</div>
+		            	:(
+		            		this.state.route === 'signin'
+			            	?<Signin onRouteChange={this.onRouteChange} />
+					      	:<Register onRouteChange={this.onRouteChange} />
+				      	)			    	
+		            }
+		    </div>
+  		);	
+	}
+}
+ 
 export default App;
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+	return (
+		<div>
+			<p className='fa3'>
+				{'This Magic brain will detect faces in your Picture. Git it a try'}
+			</p>
+			<div className='center'>
+				<div className='form center pa4 br3 shadow-5'>
+					<input className='fa4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+					<button 
+						className='w-30 grow f4 link ph3 pv2 sib white bg-light-purple' onClick={onButtonSubmit}>Detect
+					</button>
+				</div>
+			</div>
+		</div>
+	);	
+}
+
+export default ImageLinkForm;
